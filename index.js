57a2cfe881e9ab0d7a8cd2312e63777940c90162
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2017-01-12 01:49:15 +0000

    add ignore packages

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var Server = require("./lib/server");
+var Server = require('./lib/server')
 
 /**
  * Start the MQTT server
  * @param {object} config - The server configuration
  */
-exports.startServer = function(config) {
-    return new Server({ config: new ConnectionConfig(config) });
-};
+exports.startServer = function (config) {
+  return new Server({ config: new ConnectionConfig(config) })
+}
